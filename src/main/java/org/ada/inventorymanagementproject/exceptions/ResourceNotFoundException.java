9da2a46b6a1a51a1a4fa0c78fdecab5860b5fb78
lucero89 <laila.aquillos@gmail.com>
2022-11-21 15:22:15 -0300

    summaryReport sin relacion

--- src/main/java/org/ada/inventorymanagementproject/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/ada/inventorymanagementproject/exceptions/ResourceNotFoundException.java
@@ -2,7 +2,7 @@ package org.ada.inventorymanagementproject.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-    public static final String MESSAGE = "El producto que está buscando no existe.";
+    public static final String MESSAGE = "El reporte de factura que está buscando no existe.";
 
     public ResourceNotFoundException(){
 
