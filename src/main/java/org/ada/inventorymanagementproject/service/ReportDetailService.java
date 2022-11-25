aefdeef2c8cf9eb1f729eb395c4b3f71fd77d725
lucero89 <laila.aquillos@gmail.com>
2022-11-25 13:37:39 -0300

    cambios en service

--- src/main/java/org/ada/inventorymanagementproject/service/ReportDetailService.java
+++ src/main/java/org/ada/inventorymanagementproject/service/ReportDetailService.java
@@ -4,10 +4,13 @@ import org.ada.inventorymanagementproject.dto.ReportDetailDTO;
 
 import org.ada.inventorymanagementproject.entity.Item;
 import org.ada.inventorymanagementproject.entity.ReportDetail;
+import org.ada.inventorymanagementproject.entity.SummaryReport;
 import org.ada.inventorymanagementproject.exceptions.ExistingResourceException;
 import org.ada.inventorymanagementproject.exceptions.ResourceNotFoundException;
 import org.ada.inventorymanagementproject.repository.ItemRepository;
 import org.ada.inventorymanagementproject.repository.ReportDetailRepository;
+import org.ada.inventorymanagementproject.repository.SummaryReportRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -21,10 +24,12 @@ public class ReportDetailService {
 
     private final ReportDetailRepository reportDetailRepository;
     private final ItemRepository itemRepository;
+    private final SummaryReportRepository summaryReportRepository;
 
-    public ReportDetailService(ReportDetailRepository reportDetailRepository, ItemRepository itemRepository) {
+    public ReportDetailService(ReportDetailRepository reportDetailRepository, ItemRepository itemRepository, SummaryReportRepository summaryReportRepository) {
         this.reportDetailRepository = reportDetailRepository;
         this.itemRepository = itemRepository;
+        this.summaryReportRepository = summaryReportRepository;
     }
 
     public void create(ReportDetailDTO reportDetailDTO, String itemId){
@@ -81,5 +86,61 @@ public class ReportDetailService {
         reportDetailDTO.setId(reportDetail.getId());
         return reportDetailDTO;
     }
+
+
+
+    public void  create(ReportDetailDTO reportDetailDTO , Integer summaryReportId){
+        Optional<SummaryReport> summaryReport = summaryReportRepository.findById(summaryReportId);
+        if (summaryReport.isEmpty()){
+            throw new ResourceNotFoundException();
+        }
+
+        ReportDetail reportDetail = mapToEntity(reportDetailDTO , summaryReport.get());
+        reportDetail = reportDetailRepository.save(reportDetail);
+        reportDetailDTO.setId(reportDetail.getId());
+
+
+    }
+
+    public void create (List<ReportDetailDTO> reportDetailDTOS, SummaryReport summaryReport ){ //recibe ese proveedor creado en la bd, crea una lista de summary
+        List<ReportDetail> reportDetail = reportDetailDTOS.stream()
+                .map(reportDetailDTO -> mapToEntity(reportDetailDTO, summaryReport))
+                .collect(Collectors.toList());
+        reportDetailRepository.saveAll(reportDetail);
+    }
+
+    public ReportDetailDTO retrieveById(Integer summaryReportId, Integer reportDetailId) { //busca, obtiene summaryreports relacionado a un supplier
+        if (!reportDetailRepository.existsById(summaryReportId)){
+            throw new ResourceNotFoundException("El summary report no existe.");
+        }
+        Optional<ReportDetail> reportDetail = reportDetailRepository.findById(reportDetailId);
+        if (reportDetail.isEmpty()) {
+            throw new ResourceNotFoundException();
+        }
+
+        return mapToDTO(reportDetail.get());
+    }
+
+    public void delete(Integer reportDetailId) {
+        try {
+            reportDetailRepository.deleteById(reportDetailId);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException();
+        }
+    }
+
+
+    private ReportDetailDTO mapToDTO(ReportDetail reportDetail) {
+
+        ReportDetailDTO reportDetailDTO = new ReportDetailDTO(reportDetail.getId(),reportDetail.getQuantity());
+
+        return reportDetailDTO;
+    }
+    private ReportDetail mapToEntity(ReportDetailDTO reportDetailDTO , SummaryReport summaryReport) {
+        ReportDetail reportDetail = new ReportDetail(reportDetailDTO.getId(),reportDetailDTO.getQuantity());
+
+        return reportDetail;
+    }
+
 
 }
